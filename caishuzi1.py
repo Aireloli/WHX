b9fea055fd153362b4e36bb4da09b9a8f15656f5
Aireloli <1845888258@qq.com>
2020-10-25 17:31:49 +0800

    猜数字游戏改进版

--- caishuzi1.py
+++ caishuzi1.py
@@ -3,18 +3,19 @@ x=random.randint(1,100)
 count=0
 while 1>0:
     y=input("请猜一个1——100的整数:")
-    z=int(y)
-    count=count+1
-    if z==x:
-        print("你猜对了")
-        print("你猜了{}次终于猜对了".format(count))
-        break
+    if not y.isdigit():     # digit是数字的意思，isdigit就是判断是否是数字
+        print("请仔细审题哦,输入的数必须为整数")
     else:
-        if z>x:
-            print("你猜大了")
+        z=int(y)
+        count=count+1
+        if z==x:
+            print("你猜对了")
+            print("你猜了{}次终于猜对了".format(count))
+            break
         else:
-            print("你猜小了")
-            
+            if z>x:
+                print("你猜大了")
+            else:
+                print("你猜小了")
 
-            
     
