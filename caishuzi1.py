9007d2e4815303e8ed008d0cdfe9079952b72dda
老齐 <qiwsir@gmail.com>
2020-10-26 08:46:01 +0800

    Update caishuzi1.py

--- caishuzi1.py
+++ caishuzi1.py
@@ -1,8 +1,8 @@
 import random
 x=random.randint(1,100)
 count=0
-while 1>0:
-    y=input("请猜一个1——100的整数:")
+while 1>0:    # 不用这么写，只要while 1即可
+    y=input("请猜一个1——100的整数:")    # 变量名称禁止使用x, y之类的，应该用有意的单词
     if not y.isdigit():     # digit是数字的意思，isdigit就是判断是否是数字
         print("请仔细审题哦,输入的数必须为整数")
     else:
@@ -13,7 +13,7 @@ while 1>0:
             print("你猜了{}次终于猜对了".format(count))
             break
         else:
-            if z>x:
+            if z>x:   #不需要嵌套条件，多个分支即可
                 print("你猜大了")
             else:
                 print("你猜小了")
